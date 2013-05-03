3c16cc0e071b8e7ad40df1734ff39d62ec08ffbe
Dan Rollo <danrollo@gmail.com>
2013-05-02 20:23:11 -0400

    add app b (uses a.jar).

---
+++ src/main/java/org/myorg/AppB.java
@@ -0,0 +1,16 @@
+package org.myorg;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class AppB
+{
+    public static void main( String[] args )
+    {
+        App.main(null); // call class from a.jar
+
+        System.out.println( "Hello World B!" );
+    }
+}
